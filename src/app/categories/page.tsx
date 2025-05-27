e268dacae2a0dc40af0bf4ad9e782c4be5eb74a8
JangInChan <simsim051128@gmail.com>
2025-05-27 23:41:58 +0900

    fix: 동적 렌더링으로 변경

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -2,6 +2,8 @@ import { getCategories } from "@/lib/category";
 import Link from "next/link";
 import Image from "next/image";
 
+export const dynamic = "force-dynamic";
+
 export default async function CategoriesPage() {
   const categories = (await getCategories()) as any[];
 
